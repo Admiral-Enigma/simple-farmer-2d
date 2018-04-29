24e77bc7e8bdfa894ec57f5eacfd66a99522b8d7
Alexander <MrRandomgamerdkHD@gmail.com>
2018-04-29 17:17:25 +0200

    Testing out the crops

---
+++ asset.lua
@@ -0,0 +1,9 @@
+local assets = {}
+
+assets.wheat = {
+  love.graphics.newImage("res/wheat/wheat0.png"),
+  love.graphics.newImage("res/wheat/wheat1.png"),
+  love.graphics.newImage("res/wheat/wheat2.png")
+}
+
+return assets
--- main.lua
+++ main.lua
@@ -1,16 +1,19 @@
 local Tilemap = require "core.Tilemap"
 local Picker = require "game.Picker"
 local Hud = require "game.ui.hud"
+local Crop = require "game.crops.Crop"
 Camera = require "core.lib.hump.camera"
 
 
 function love.load(arg)
   love.keyboard.setKeyRepeat(true)
   love.graphics.setDefaultFilter("nearest", "nearest")
-
+  Assets = require "asset"
 
   mapData = require "res.TestMap"
   map = Tilemap:new(32, 32, 100, 100, mapData)
+
+  testCrop = Crop:new("Fisksild", 32, 32, 10, Assets.wheat)
   picker = Picker:new(map)
   hud = Hud:new(picker)
   camera = Camera.new(picker:getX(), picker:getY(), 1)
@@ -24,9 +27,10 @@ end
 
 function love.draw()
   love.graphics.setColor(53,99,35)
-  love.graphics.rectangle("fill", 0, 0, 800, 600)
+  love.graphics.rectangle("fill", 0, 0, 2000, 2000)
   camera:attach()
   map:draw()
+  testCrop:draw()
   picker:draw()
   camera:detach()
   hud:draw()
@@ -37,5 +41,8 @@ function love.keypressed(key, scancode, isrepeat)
   picker:keypressed(key, scancode, isrepeat)
   if key == "q" then
     love.event.quit()
+  end
+  if key == "p" then
+    testCrop:tick()
   end
 end
