002271be550833fe9243f1be977a24368ec18c6b
Alexander <MrRandomgamerdkHD@gmail.com>
2018-05-04 17:34:01 +0200

    Added wheat blueprint

--- game/item/ItemBlueprints.lua
+++ game/item/ItemBlueprints.lua
@@ -4,11 +4,19 @@ local blueprints = {}
   - name
   - price
   - description
-  - itemType # 0 = usable (seed, fertilizer) 1 = nonUsable(harvested crops, special items) 2 = placeAble(furniture and miscellaneous objects)
+  - type # 0 = usable (seed, fertilizer) 1 = nonUsable(harvested crops, special items) 2 = placeAble(furniture and miscellaneous objects)
   - sellAble
   #Optional
   - Icon
   -
 ]]
+
+blueprints.wheat = {
+  name = "Wheat",
+  price = 10,
+  description = "Very wheaty much taste",
+  type = 1,
+  sellAble = true
+}
 
 return blueprints
