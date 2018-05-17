24337013ec3ee0420e0189fbfe38c4369e356807
Alexander <MrRandomgamerdkHD@gmail.com>
2018-05-17 17:05:41 +0200

    Added new util functions

--- utils/Color.lua
+++ utils/Color.lua
@@ -2,3 +2,6 @@
 function rgb (r, g, b)
   return {r / 255, g / 255, b / 255}
 end
+function rgba (r, g, b, a)
+  return {r / 255, g / 255, b / 255, a / 255}
+end
--- utils/TileTools.lua
+++ utils/TileTools.lua
@@ -1,6 +1,7 @@
 
-function drawOutlineTile (col, row, width, height)
-  love.graphics.setColor(0, 255, 0)
+function drawOutlineTile (col, row, width, height, color)
+  if not color then color = rgb(0,255,0) end
+  love.graphics.setColor(color)
   love.graphics.rectangle("line", col * width, row * height, width, height)
 end
 
